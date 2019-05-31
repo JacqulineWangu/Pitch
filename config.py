19629ccef31d3de90a66ed4536cd02f2cbd20b0a
jacquline <jacqulinewangu@gmail.com>
2019-05-31 11:46:02 +0300

    deployment

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ class Config:
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://jacquline:jaki@localhost/pitchi'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    UPLOADED_PHOTOS_DEST ='app/static/img'
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
+    SENDER_EMAIL=os.environ.get("MAIL_USERNAME")
 
 class ProdConfig(Config):
     '''
