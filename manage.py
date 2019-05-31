a33b149709752c80c09a58ade335d1eabb346a90
jacquline <jacqulinewangu@gmail.com>
2019-05-31 10:43:01 +0300

    migrating

--- manage.py
+++ manage.py
@@ -3,7 +3,9 @@ from flask_script import Manager,Server
 from flask_migrate import Migrate,MigrateCommand
 from app.models import User,Pitch,Comment
 
-
+#Create app instance
+# app = create_app('test')
+# app = create_app('production')
 app = create_app('production')
 
 manager = Manager(app)
