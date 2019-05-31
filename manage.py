834b061ccf9ddf35ebddb1ab00512e1558bb9a63
jacquline <jacqulinewangu@gmail.com>
2019-05-31 10:36:27 +0300

    redeployment

--- manage.py
+++ manage.py
@@ -3,10 +3,8 @@ from flask_script import Manager,Server
 from flask_migrate import Migrate,MigrateCommand
 from app.models import User,Pitch,Comment
 
-#Create app instance
-# app = create_app('test')
-# app = create_app('production')
-app = create_app('development')
+
+app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server',Server)
